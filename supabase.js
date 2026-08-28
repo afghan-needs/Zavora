ad7508b862d052cd253117fa6df9d15fdcbf6c1c
afghan-needs <zabulalamyar55@gmail.com>
2026-08-29 03:24:14 +0430

    Initialize Supabase client with URL and anon key

--- supabase.js
+++ supabase.js
@@ -1,1 +1,9 @@
 
+const SUPABASE_URL = 'https://lpxqytvxkspisynnxclx.supabase.co';
+
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxweHF5dHZ4a3NwaXN5bm54Y2x4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODc3MzQ4NzgsImV4cCI6MjEwMzMxMDg3OH0.K1BvP5Sw6b1XNKz7FEXDjvc0ZQf8SRu4wBkE2mErRqQ';
+
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_ANON_KEY
+);
